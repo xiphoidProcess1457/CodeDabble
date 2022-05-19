39ea103df9e863b621cf843630104f028adbb961
Bryan Aaron Sta Teresa <74145639+darkseidcloud30@users.noreply.github.com>
2022-05-19 17:33:20 +0800

    fix sidebar moderator access

--- app/Views/sidebar.php
+++ app/Views/sidebar.php
@@ -34,21 +34,11 @@
             <?php if (session()->get('role') =='moderator' ):?>
        
           
-              <li>
-              <a href="<?= base_url('Admin/addlesson');?>" class="nav-link text-white sidebar-links">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-                  ADD LESSON
-                </a>
-              </li>
+           
               
             
              
-              <li>
-                <a href="<?= base_url('Admin/users');?>" class="nav-link text-white sidebar-links">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
-                  MANAGE USERS
-                </a>
-              </li>
+             
               <li>
                 <a href="<?= base_url('Admin/logout');?>" class="nav-link text-white sidebar-links">
                   <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
